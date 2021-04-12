450032a18bacd97636408df2c17a73b37ebd9d2b
Clara Martins <up201806528@fe.up.pt>
2021-04-12 08:26:32 +0100

    Thread is created at answer so both answer types are on the same thread

--- proj1/src/peer/messages/RestoreSenderMessage.java
+++ proj1/src/peer/messages/RestoreSenderMessage.java
@@ -35,81 +35,81 @@ public class RestoreSenderMessage extends Message {
 
     @Override
     public void answer(int id) {
-        if (this.version.equals("1.0")) {
-            this.regularAnswer(id);
-        }
-        else {
-            this.enhancedAnswer(id);
-        }
+        // New Thread to deal with the answer
+        Thread thread = new Thread(() -> {
+            if (this.version.equals("1.0")) {
+                this.regularAnswer(id);
+            }
+            else {
+                this.enhancedAnswer(id);
+            }
+        });
+        thread.start();
     }
 
     public void regularAnswer(int id) {
-        // New Thread to deal with the answer
-        Thread thread = new Thread(() -> {
-            // Search for the chunkContent
-            byte[] chunkContent = Utils.load(id, this.fileId, this.chunkNo);
-            if (chunkContent == null) {
-                return;
-            }
+        // Search for the chunkContent
+        byte[] chunkContent = Utils.load(id, this.fileId, this.chunkNo);
+        if (chunkContent == null) {
+            return;
+        }
 
-            // Get message
-            Message message = new RestoreReceiverMessage(this.mc, this.mdb, this.mdr, this.version, id, this.fileId, this.chunkNo, chunkContent);
+        // Get message
+        Message message = new RestoreReceiverMessage(this.mc, this.mdb, this.mdr, this.version, id, this.fileId, this.chunkNo, chunkContent);
 
-            // Open MDR Socket
-            MulticastSocket socket;
-            try {
-                socket = new MulticastSocket(this.mdr.port);
-            }
-            catch (IOException exception) {
-                PeerDebugger.println("Error occurred");
-                return;
-            }
-            try {
-                socket.joinGroup(this.mdr.ip);
-            }
-            catch (IOException exception) {
-                PeerDebugger.println("Error occurred: " + exception.getMessage());
-                socket.close();
-                return;
-            }
-
-            // Verify if no other peer is faster to send the chunk
-            byte[] buf = new byte[Message.MESSAGE_SIZE];
-            DatagramPacket p = new DatagramPacket(buf, buf.length);
-            long initial_timestamp = System.currentTimeMillis();
-            long current_timestamp = System.currentTimeMillis();
-            int timeToWait = Utils.getRandomNumber(0, 401);
-            boolean answerReceived = false;
-            while (current_timestamp < initial_timestamp + timeToWait) {
-                try {
-                    socket.setSoTimeout((int) (initial_timestamp + timeToWait - current_timestamp));
-                    socket.receive(p);
-                    Message answer = Message.parse(mc, mdb, mdr, p);
-                    if ((answer instanceof RestoreReceiverMessage) && (((RestoreReceiverMessage) answer).correspondsTo(fileId, chunkNo))) {
-                        answerReceived = true;
-                        break;
-                    }
-                }
-                catch (SocketTimeoutException exception) {
-                    // The time expired and no message was received
-                }
-                catch (IOException ignored) { }
-                current_timestamp = System.currentTimeMillis();
-            }
-
-            // Send chunk if needed
-            if (!answerReceived) {
-                Utils.sendMessage(message);
-            }
-
-            // Close MDR Socket
-            try {
-                socket.leaveGroup(this.mdr.ip);
-            }
-            catch (Exception ignored) { }
+        // Open MDR Socket
+        MulticastSocket socket;
+        try {
+            socket = new MulticastSocket(this.mdr.port);
+        }
+        catch (IOException exception) {
+            PeerDebugger.println("Error occurred");
+            return;
+        }
+        try {
+            socket.joinGroup(this.mdr.ip);
+        }
+        catch (IOException exception) {
+            PeerDebugger.println("Error occurred: " + exception.getMessage());
             socket.close();
-        });
-        thread.start();
+            return;
+        }
+
+        // Verify if no other peer is faster to send the chunk
+        byte[] buf = new byte[Message.MESSAGE_SIZE];
+        DatagramPacket p = new DatagramPacket(buf, buf.length);
+        long initial_timestamp = System.currentTimeMillis();
+        long current_timestamp = System.currentTimeMillis();
+        int timeToWait = Utils.getRandomNumber(0, 401);
+        boolean answerReceived = false;
+        while (current_timestamp < initial_timestamp + timeToWait) {
+            try {
+                socket.setSoTimeout((int) (initial_timestamp + timeToWait - current_timestamp));
+                socket.receive(p);
+                Message answer = Message.parse(mc, mdb, mdr, p);
+                if ((answer instanceof RestoreReceiverMessage) && (((RestoreReceiverMessage) answer).correspondsTo(fileId, chunkNo))) {
+                    answerReceived = true;
+                    break;
+                }
+            }
+            catch (SocketTimeoutException exception) {
+                // The time expired and no message was received
+            }
+            catch (IOException ignored) { }
+            current_timestamp = System.currentTimeMillis();
+        }
+
+        // Send chunk if needed
+        if (!answerReceived) {
+            Utils.sendMessage(message);
+        }
+
+        // Close MDR Socket
+        try {
+            socket.leaveGroup(this.mdr.ip);
+        }
+        catch (Exception ignored) { }
+        socket.close();
     }
 
     public void enhancedAnswer(int id) {
